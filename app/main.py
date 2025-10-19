c65cf1870a397cee80ababcca80f57a1aed98691
Mouleas <mouleaswars@presidio.com>
2025-10-19 18:02:04 +0530

    integrated db

---
+++ app/main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+
+from database import SessionLocal, engine, Base
+from model import Item
+from schema import ItemCreate, ItemResponse
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/healthy")
+def check_healthy():
+    return {"status": "OK", "message": "Your application is healthy!"}
+
+
+@app.get("/")
+def read_root():
+    return {"status": "OK", "message": "Welcome to the FastAPI application!"}
+
+
+@app.get("/items/", response_model=List[ItemResponse])
+def read_items(db: Session = Depends(get_db)):
+    items = db.query(Item).all()
+    return items
+
+
+@app.post("/items/", response_model=ItemResponse)
+def create_item(item: ItemCreate, db: Session = Depends(get_db)):
+    db_item = Item(name=item.name)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+
+@app.get("/items/{item_id}", response_model=ItemResponse)
+def read_item_by_id(item_id: int, db: Session = Depends(get_db)):
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
---
+++ app/schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class ItemBase(BaseModel):
+    name: str
+
+class ItemCreate(ItemBase):
+    pass
+
+class ItemResponse(ItemBase):
+    id: int
+
+    class Config:
+        orm_mode = True
