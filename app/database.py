dfb4cf277884c1df8bde5e92192fe68a3120b0b4
Mouleas <mouleaswars@presidio.com>
2025-10-19 18:23:08 +0530

    modified db name

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:root1234@db-items.cdgkfoacvf6u.us-east-1.rds.amazonaws.com:5432/db-items"
+DATABASE_URL = "postgresql://postgres:root1234@db-items.cdgkfoacvf6u.us-east-1.rds.amazonaws.com:5432/"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
